206597398368750a91452e58c3c2747ccdf776e0
msh2481 <msh2481@mail.ru>
2023-06-22 22:43:31 +0100

    It works

--- client.py
+++ client.py
@@ -9,5 +9,7 @@ def post(request):
     return requests.post("http://etrade.pythonanywhere.com/", json=request)
 
 
-print(post({"command": "register"}).json())
-print(post({"command": "buy", "symbol": "A", "price": 10.0, "volume": 1.0}).json())
+# print(post({"command": "register"}).json())
+# print(post({"command": "buy", "symbol": "A", "price": 10.0, "volume": 1.0}).json())
+print(post({"command": "book"}).json())
+print(post({"command": "account"}).json())
