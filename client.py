eb06a1568a942686f44134bccab4764b6deb9faa
msh2481 <msh2481@mail.ru>
2023-06-22 22:35:21 +0100

    Another server fix

--- client.py
+++ client.py
@@ -9,7 +9,9 @@ def post(request):
     return requests.post("http://etrade.pythonanywhere.com/", json=request)
 
 
-response = post({"command": "register"})
-print(response, response.json())
-response = post({"command": "account"})
-print(response, response.json())
+# response = post({"command": "register"})
+# print(response, response.json())
+# response = post({"command": "account"})
+# print(response, response.json())
+
+print(post({"command": "buy", "symbol": "A", "price": 10.0, "volume": 1.0}).json())
--- server.py
+++ server.py
@@ -32,12 +32,12 @@ def handle_request():
             response = accounts[account].to_dict()
         elif command == "buy":
             assert account in accounts
-            order = Order(symbol=command["symbol"], sign=BUY, price=command["price"], volume=command["volume"], account=account)
+            order = Order(symbol=data["symbol"], sign=BUY, price=data["price"], volume=data["volume"], account=account)
             books.put_order(order)
             response = {"status": "ok"}
         elif command == "sell":
             assert account in accounts
-            order = Order(symbol=command["symbol"], sign=SELL, price=command["price"], volume=command["volume"], account=account)
+            order = Order(symbol=data["symbol"], sign=SELL, price=data["price"], volume=data["volume"], account=account)
             books.put_order(order)
             response = {"status": "ok"}
         elif command == "register":
